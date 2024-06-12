51750961c20bed39f64c65e76b286bc798e520c9
Dev maheshwari <devmaheswari2017@gmail.com>
2024-06-12 15:35:15 +0530

    Shooting bullets using bullet pool now

---
+++ Assets/Scripts/Bullets/BulletPool.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace CosmicCuration.Bullets {
+
+    public class BulletPool
+    {
+        private BulletView bulletView;
+        private BulletScriptableObject bulletScriptableObject;
+        private List<PooledBullet> pooledBullets = new List<PooledBullet>();
+        public BulletPool(BulletView bulletView, BulletScriptableObject bulletScriptableObject)
+        {
+            this.bulletView = bulletView;
+            this.bulletScriptableObject = bulletScriptableObject;
+        }
+
+        public BulletController GetBullet() 
+        {
+            if (pooledBullets.Count > 0) 
+            {
+                PooledBullet pooledBullet = pooledBullets.Find(item => !item.isUsed);
+                if (pooledBullet != null) 
+                {
+                    pooledBullet.isUsed = true;
+                    return pooledBullet.bullet;
+                }
+            }
+            return CreateNewPooledBullet();
+        }
+        private BulletController CreateNewPooledBullet() 
+        {
+            PooledBullet pooledBullet = new PooledBullet();
+            pooledBullet.bullet = new BulletController(bulletView, bulletScriptableObject);
+            pooledBullet.isUsed = true;
+
+            return pooledBullet.bullet;
+        }
+        public class PooledBullet
+        {
+            public bool isUsed;
+            public BulletController bullet;
+        }
+    }
+}
